1e4225dca6207df0bbd12799206a148d5288bafb
er-sunilsharma <sharmasunil0968gmail.com>
2024-08-13 16:39:16 +0530

    update design

--- app/figma/page.js
+++ app/figma/page.js
@@ -22,7 +22,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/qyIY1"
+                  href="https://www.figma.com/proto/IhwObTdps693GzqwZ8QEqy/Super-Kirana-App-Dashboard?page-id=0%3A1&node-id=12-95&starting-point-node-id=12%3A95&t=uOn0p8VNhIh8dPxu-1"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -38,7 +38,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/bcwH0"
+                  href="https://www.figma.com/proto/aGeVD5u8BiOHy3EMgMMCEU/Vaccination-India-App?page-id=0%3A1&node-id=1-9&viewport=570%2C1824%2C0.4&t=O04TA4Jjo7VzbjBi-1&scaling=contain&content-scaling=fixed&starting-point-node-id=1%3A9"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -56,7 +56,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/dCIKY"
+                  href="https://www.figma.com/proto/FSIl07pxhUVORFVC1hbo4n/BDA-Media?page-id=0%3A1&node-id=142-160&viewport=0%2C500%2C0.13&t=I6Um0lTJi3oTD6Tx-1&scaling=min-zoom&content-scaling=fixed"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -74,7 +74,97 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/UJ1TX"
+                  href="https://www.figma.com/proto/gDYFQLcOvyoht2yX4OEQkg/Online-Bus-Booking-App?page-id=0%3A1&node-id=1-2&viewport=233%2C371%2C0.96&t=MGuZDriqxHguO75d-1&scaling=scale-down&content-scaling=fixed"
+                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
+                >
+                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
+                    <LuFigma size={24} />
+                  </span>
+                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
+                </Link>
+              </div>
+              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
+                <div className="w-full mb-4">
+                  <h2 className="text-lg font-medium">
+                    RedBus App Design Clone
+                  </h2>
+                </div>
+                <Link
+                  target="_blank"
+                  rel="nofollow"
+                  href="https://www.figma.com/proto/gDYFQLcOvyoht2yX4OEQkg/Online-Bus-Booking-App?page-id=0%3A1&node-id=1-2&viewport=233%2C371%2C0.96&t=MGuZDriqxHguO75d-1&scaling=scale-down&content-scaling=fixed"
+                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
+                >
+                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
+                    <LuFigma size={24} />
+                  </span>
+                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
+                </Link>
+              </div>
+              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
+                <div className="w-full mb-4">
+                  <h2 className="text-lg font-medium">
+                    Financial Market Template
+                  </h2>
+                </div>
+                <Link
+                  target="_blank"
+                  rel="nofollow"
+                  href="https://www.figma.com/proto/qr9nzQTZjvdQBZffzEIiXE/Best-Financial-Market?page-id=0%3A1&node-id=3-3&viewport=286%2C347%2C0.13&t=BU57QEGa61rAlyle-1&scaling=min-zoom&content-scaling=fixed"
+                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
+                >
+                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
+                    <LuFigma size={24} />
+                  </span>
+                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
+                </Link>
+              </div>
+              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
+                <div className="w-full mb-4">
+                  <h2 className="text-lg font-medium">
+                    Dark Empire Market Template
+                  </h2>
+                </div>
+                <Link
+                  target="_blank"
+                  rel="nofollow"
+                  href="https://www.figma.com/proto/qr9nzQTZjvdQBZffzEIiXE/Best-Financial-Market?page-id=0%3A1&node-id=27-2&viewport=286%2C347%2C0.13&t=BU57QEGa61rAlyle-1&scaling=min-zoom&content-scaling=fixed"
+                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
+                >
+                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
+                    <LuFigma size={24} />
+                  </span>
+                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
+                </Link>
+              </div>
+              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
+                <div className="w-full mb-4">
+                  <h2 className="text-lg font-medium">
+                    Financial Shopping Template
+                  </h2>
+                </div>
+                <Link
+                  target="_blank"
+                  rel="nofollow"
+                  href="https://www.figma.com/proto/qr9nzQTZjvdQBZffzEIiXE/Best-Financial-Market?page-id=0%3A1&node-id=31-358&viewport=286%2C347%2C0.13&t=BU57QEGa61rAlyle-1&scaling=min-zoom&content-scaling=fixed"
+                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
+                >
+                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
+                    <LuFigma size={24} />
+                  </span>
+                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
+                </Link>
+              </div>
+              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
+                <div className="w-full mb-4">
+                  <h2 className="text-lg font-medium">
+                    Financial Market Template 02
+                  </h2>
+                </div>
+                <Link
+                  target="_blank"
+                  rel="nofollow"
+                  href="https://www.figma.com/proto/qr9nzQTZjvdQBZffzEIiXE/Best-Financial-Market?page-id=0%3A1&node-id=45-597&viewport=286%2C347%2C0.13&t=BU57QEGa61rAlyle-1&scaling=min-zoom&content-scaling=fixed"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -97,7 +187,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/nrsIX"
+                  href="https://drive.google.com/drive/folders/10grlC6xSCUNnbJF289A_wlEBW8YwRmpR"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-purple-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -156,7 +246,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/nLOWZ"
+                  href="https://www.figma.com/proto/Hg123e1qPfhPiMw5xIgueL/BDA-Corp-Logo?page-id=0%3A1&node-id=1-2&viewport=658%2C511%2C1&t=seO6TuZD8oejTWXV-1&scaling=min-zoom&content-scaling=fixed"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-purple-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -172,7 +262,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/DP045"
+                  href="https://www.figma.com/proto/vBkHYpaZI0SIvtYwYP3RED/BdaTrip-Logo?page-id=0%3A1&node-id=1-13&viewport=413%2C483%2C0.69&t=BmB80v7Y1NASvEK1-1&scaling=contain&content-scaling=fixed"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-purple-600 group-hover:bg-purple-600 flex items-center justify-center">
@@ -188,7 +278,7 @@ export default function About() {
                 <Link
                   target="_blank"
                   rel="nofollow"
-                  href="https://shorturl.at/uESV4"
+                  href="https://www.figma.com/proto/TOo1C0MtxkTWnetWCOKJao/Cuccu-Logo-Design?page-id=0%3A1&node-id=1-4&t=7tK8D2xoOjlue73P-1"
                   class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
                 >
                   <span class="px-3.5 py-2 text-white bg-purple-600 group-hover:bg-purple-600 flex items-center justify-center">
