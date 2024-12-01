08102b23e61b70d4895c12687844ba1ec290373c
er-sunilsharma <sharmasunil0968gmail.com>
2024-12-01 20:25:22 +0530

    i have update

--- app/figma/page.js
+++ app/figma/page.js
@@ -65,24 +65,6 @@ export default function About() {
                   <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
                 </Link>
               </div>
-              <div className="w-full bg-white rounded-2xl pt-5 pb-7 px-4 text-center shadow-2xl">
-                <div className="w-full mb-4">
-                  <h2 className="text-lg font-medium">
-                    RedBus App Design Clone
-                  </h2>
-                </div>
-                <Link
-                  target="_blank"
-                  rel="nofollow"
-                  href="https://www.figma.com/proto/gDYFQLcOvyoht2yX4OEQkg/Online-Bus-Booking-App?page-id=0%3A1&node-id=1-2&viewport=233%2C371%2C0.96&t=MGuZDriqxHguO75d-1&scaling=scale-down&content-scaling=fixed"
-                  class="inline-flex overflow-hidden text-white bg-gray-900 rounded-full group"
-                >
-                  <span class="px-3.5 py-2 text-white bg-red-600 group-hover:bg-purple-600 flex items-center justify-center">
-                    <LuFigma size={24} />
-                  </span>
-                  <span class="pl-4 pr-5 py-2.5 font-medium">Figma Design</span>
-                </Link>
-              </div>
             </div>
           </div>
           <div className="w-full">
